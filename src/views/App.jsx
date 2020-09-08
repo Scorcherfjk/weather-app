e2268761ac6c3fa02b543afc81b67d280b08ecd2
Fjavier De Freitas <franjavikale@gmail.com>
2020-09-08 18:32:00 -0500

    estilos globales y contenedor

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
+
+import Sidebar from '../components/Sidebar';
+import Content from '../components/Content';
+
 import '../assets/styles/App.scss';
 
-const App = () => <div>Hello world</div>;
+const App = () => (
+  <div className="app">
+    <Sidebar />
+    <Content />
+  </div>
+);
 
 export default App;
