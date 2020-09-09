d339507c2ba484fc767387604e0d4f34ccc75657
Fjavier De Freitas <franjavikale@gmail.com>
2020-09-09 12:52:14 -0500

    arreglando tarjetas inferiores

--- src/components/LargeCard.jsx
+++ src/components/LargeCard.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 
 import '../assets/styles/LargeCard.scss';
 
-const LargeCard = ({ title, detail, optional }) => (
+const LargeCard = ({
+  title, detail, metric, children,
+}) => (
   <div className="large-card">
     <h4 className="large-card__title">{title}</h4>
-    <div className="large-card__detail">{detail}</div>
-    <div className="large-card__optional">{optional}</div>
+    <div className="large-card__detail">
+      {detail}
+      <span>{metric}</span>
+    </div>
+    <div className="large-card__optional">{children}</div>
   </div>
 );
 
--- src/components/MediumCard.jsx
+++ src/components/MediumCard.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 
 import '../assets/styles/MediumCard.scss';
 
-const MediumCard = ({ title, detail }) => (
+const MediumCard = ({ title, detail, metric }) => (
   <div className="medium-card">
     <h4 className="medium-card__title">{title}</h4>
-    <div className="medium-card__detail">{detail}</div>
+    <div className="medium-card__detail">
+      {detail}
+      <span>{metric}</span>
+    </div>
   </div>
 );
 
