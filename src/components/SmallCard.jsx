28410b06ed8323ccc7aabb9f07e738e7fa8af2ca
Fjavier De Freitas <franjavikale@gmail.com>
2020-09-09 12:53:14 -0500

    cambio de la tarjeta pequeña

--- src/components/SmallCard.jsx
+++ src/components/SmallCard.jsx
@@ -6,13 +6,24 @@ const SmallCard = ({
   img, date, min, max, grades,
 }) => {
   const fecha = new Date(date);
+  let minT;
+  let maxT;
+
+  if (grades === 'C') {
+    minT = (min - 273.15).toFixed(2);
+    maxT = (max - 273.15).toFixed(2);
+  } else {
+    minT = min;
+    maxT = max;
+  }
+
   return (
     <div className="small-card">
       <h4 className="small-card__date">{fecha.toDateString()}</h4>
       <img className="small-card__image" src={img} alt="" />
       <ul className="small-card__list">
-        <li>{min + grades}</li>
-        <li>{max + grades}</li>
+        <li>{`${minT}°${grades}`}</li>
+        <li>{`${maxT}°${grades}`}</li>
       </ul>
     </div>
   );
