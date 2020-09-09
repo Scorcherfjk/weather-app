e00d10a657acab4977e1327cc3808b15747b46c6
Fjavier De Freitas <franjavikale@gmail.com>
2020-09-09 09:20:16 -0500

    get props

--- src/components/LargeCard.jsx
+++ src/components/LargeCard.jsx
@@ -2,15 +2,12 @@ import React from 'react';
 
 import '../assets/styles/LargeCard.scss';
 
-const LargeCard = () => {
-  const fecha = new Date();
-  return (
-    <div className="large-card">
-      <h4 className="large-card__title">{fecha.toDateString()}</h4>
-      <div className="large-card__detail">detalle</div>
-      <div className="large-card__optional">nada</div>
-    </div>
-  );
-};
+const LargeCard = ({ title, detail, optional }) => (
+  <div className="large-card">
+    <h4 className="large-card__title">{title}</h4>
+    <div className="large-card__detail">{detail}</div>
+    <div className="large-card__optional">{optional}</div>
+  </div>
+);
 
 export default LargeCard;
--- src/components/MediumCard.jsx
+++ src/components/MediumCard.jsx
@@ -2,14 +2,11 @@ import React from 'react';
 
 import '../assets/styles/MediumCard.scss';
 
-const MediumCard = () => {
-  const fecha = new Date();
-  return (
-    <div className="medium-card">
-      <h4 className="medium-card__title">{fecha.toDateString()}</h4>
-      <div className="medium-card__detail">detalle</div>
-    </div>
-  );
-};
+const MediumCard = ({ title, detail }) => (
+  <div className="medium-card">
+    <h4 className="medium-card__title">{title}</h4>
+    <div className="medium-card__detail">{detail}</div>
+  </div>
+);
 
 export default MediumCard;
