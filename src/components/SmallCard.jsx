70bda7ebe896a0d8b7ec5d547a2ee879f0346906
Fjavier De Freitas <franjavikale@gmail.com>
2020-09-09 10:13:35 -0500

    aceptando componentes

--- src/components/SmallCard.jsx
+++ src/components/SmallCard.jsx
@@ -3,16 +3,19 @@ import React from 'react';
 import '../assets/styles/SmallCard.scss';
 
 const SmallCard = ({
-  img, date, min, max,
-}) => (
-  <div className="small-card">
-    <h4 className="small-card__date">{date}</h4>
-    <img className="small-card__image" src={img} alt="" />
-    <ul className="small-card__list">
-      <li>{min}</li>
-      <li>{max}</li>
-    </ul>
-  </div>
-);
+  img, date, min, max, grades,
+}) => {
+  const fecha = new Date(date);
+  return (
+    <div className="small-card">
+      <h4 className="small-card__date">{fecha.toDateString()}</h4>
+      <img className="small-card__image" src={img} alt="" />
+      <ul className="small-card__list">
+        <li>{min + grades}</li>
+        <li>{max + grades}</li>
+      </ul>
+    </div>
+  );
+};
 
 export default SmallCard;
